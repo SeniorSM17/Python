8233588e34cb46a8a99a19fce70028dc30c89126
SeniorSM17 <sm22227@corp.ojas-it.com>
2022-08-22 18:30:38 +0530

    Exercise 22 08 22

---
+++ BasicQuestions/Exercises_21-08-2022.py
@@ -0,0 +1,24 @@
+#   5-Exercises
+#   ------------
+
+#1.With a given integral number n, write a program to generate a dictionary that contains (i, i**3) such that is an integral number between 1 and n (both included).
+# and then the program should print the dictionary.
+"""output: 1:1  2:8 3:27... depends on the given number"""
+
+
+#2. WAP to find hexa format of decimal number, the decimal number is taken from the user
+""" ex:- input: 25
+        output: 19
+          and
+          input:5
+          output: 5"""
+
+
+#3.Write a program to merge two string, two string elements taken from the user?
+""" input: str1=abr
+           str2=ka
+    output: akbar    """
+
+
+
+
